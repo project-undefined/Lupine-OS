f2e1c2364dd0e8f16f4d3796756f75fba25acaf1
PitchBlackNights <vantassell.blake111@gmail.com>
2023-03-06 16:43:52 -0600

    fix crates, vga_buffer system, configure macros

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,20 @@
-// src/main.rs
-// When using VSCode, with the 'rust-analyzer' extenstion
-// there will be a pereptual error at the first character
-// this is because 'rust-analyzer' gets confused when you
-// don't use rust normally
-
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    panic!("Test of panic system");
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
-// this function is the entry point, since the linker looks for a function
-// named `_start` by default
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
